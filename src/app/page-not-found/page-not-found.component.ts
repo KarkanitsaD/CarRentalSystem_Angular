c26eccb1b37e705af2ac670aae61ea8d96f597bd
ICX\dzmitry.karkanitsa <dzmitry.karkanitsa@itechart-group.com>
2021-11-03 14:26:23 +0300

    code fixing

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from "@angular/core";
     selector: 'app-page-not-found',
     template: `<h3>Page not found!</h3>`
 })
-export class PageNotFoundComponent implements OnInit{
+export class PageNotFoundComponent implements OnInit {
     constructor() {}
 
     ngOnInit(){
