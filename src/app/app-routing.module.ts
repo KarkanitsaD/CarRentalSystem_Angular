2deb63fd87f64e9275797c8a8a2a44ece3fd5088
ICX\dzmitry.karkanitsa <dzmitry.karkanitsa@itechart-group.com>
2021-11-04 16:44:14 +0300

    create constans for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from 'src/app/components/login/login.component';
 import { RegisterComponent } from 'src/app/components/register/register.component';
 import { PageNotFoundComponent } from 'src/app/shared/components/page-not-found/page-not-found.component';
+import { LOGIN_PAGE_PATH, REGISTER_PAGE_PATH } from './core/constants/page-constans';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
+  { path: LOGIN_PAGE_PATH, component: LoginComponent},
+  { path: REGISTER_PAGE_PATH, component: RegisterComponent},
   { path: '**', component: PageNotFoundComponent}
 ];
 
