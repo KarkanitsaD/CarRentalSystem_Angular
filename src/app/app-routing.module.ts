1785eb48f419b90b49d693ac997fd168c5b604af
ICX\dzmitry.karkanitsa <dzmitry.karkanitsa@itechart-group.com>
2021-11-11 12:57:37 +0300

    create template for adding new car

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { CarListComponent } from './components/car/car-list/car-list.component';
 import { LoginComponent } from 'src/app/components/auth/login/login.component';
 import { RegisterComponent } from 'src/app/components/auth/register/register.component';
 import { PageNotFoundComponent } from 'src/app/shared/components/page-not-found/page-not-found.component';
-import { CARLIST_PAGE_PATH, PROFILE_PAGE, LOGIN_PAGE_PATH, MAIN_PAGE_PATH, MANAGEMENT_PAGE_PATH, NO_PERMISSION_PATH, PAGE_NOT_FOUND_PATH, REGISTER_PAGE_PATH } from './core/constants/page-constans';
+import { CARLIST_PAGE_PATH, PROFILE_PAGE, LOGIN_PAGE_PATH, MAIN_PAGE_PATH, MANAGEMENT_PAGE_PATH, NO_PERMISSION_PATH, PAGE_NOT_FOUND_PATH, REGISTER_PAGE_PATH, ADD_CAR_PAGE_PATH } from './core/constants/page-constans';
 import { ManagementComponent } from './components/manager/management/management.component';
 import { OnlyAdminGuard } from './core/guards/only-admin-guard';
 import { NoPermissionComponent } from './shared/components/no-permission/no-permission.component';
 import { MainPageComponent } from './shared/components/main-page/main-page.component';
 import { HomeComponent } from './components/account/home/home.component';
 import { AuthorizedGuard } from './core/guards/authorized.guard';
+import { AddCarComponent } from './components/car/add-car/add-car.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/rider', pathMatch: 'full'},
@@ -20,6 +21,7 @@ const routes: Routes = [
   { path: REGISTER_PAGE_PATH, component: RegisterComponent},
   { path: CARLIST_PAGE_PATH, component: CarListComponent },
   { path: MANAGEMENT_PAGE_PATH, component: ManagementComponent, canActivate: [OnlyAdminGuard] },
+  { path: ADD_CAR_PAGE_PATH, component: AddCarComponent, canActivate: [OnlyAdminGuard] },
   { path: NO_PERMISSION_PATH, component: NoPermissionComponent },
   { path: PAGE_NOT_FOUND_PATH, component: PageNotFoundComponent},
 ];
@@ -29,4 +31,13 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [LoginComponent, RegisterComponent, PageNotFoundComponent, ManagementComponent, CarListComponent, MainPageComponent, HomeComponent]
+export const routingComponents = [
+  LoginComponent,
+  RegisterComponent,
+  PageNotFoundComponent,
+  ManagementComponent,
+  CarListComponent,
+  MainPageComponent,
+  HomeComponent,
+  AddCarComponent,
+]
---
+++ src/app/components/car/add-car/add-car.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-add-car',
+    templateUrl: './add-car.component.html',
+    styleUrls: ['./add-car.component.css']
+})
+export class AddCarComponent {
+    constructor
+    (
+
+    ) {}
+    
+}
