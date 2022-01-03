e212365d99b678aad680f39041cbb6fdf075dfc1
Dmitry Karkanitsa <aakarkanica@gmail.com>
2022-01-03 21:55:39 +0300

    Create tim-range store.

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,13 +1,14 @@
 import { ActionReducerMap, MetaReducer } from "@ngrx/store";
 import { environment } from "src/environments/environment";
+import { timeRange, timeRangeReducer, TimeRangeState } from "./time-range";
 
 
 export interface State {
-
+    [timeRange]: TimeRangeState
 }
 
 export const reducers: ActionReducerMap<State> = {
-    
+    [timeRange]: timeRangeReducer
 };
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
